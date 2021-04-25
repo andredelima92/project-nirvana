96d306533dac1a41a77e9a999f22876dbee514e2
Andre <andredelima92@gmail.com>
2021-04-25 15:49:53 -0300

    feature: Dto required datas

--- server/Dtos/TravelCreateDto.cs
+++ server/Dtos/TravelCreateDto.cs
@@ -1,15 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace server.Dtos
 {
     public class TravelCreateDto
-    {
+    {   
+        [Required(ErrorMessage = "Campo nome é obrigatorio")]
         public string Name {get; set;}
         
+        [Required(ErrorMessage = "Campo uf é obrigatorio")]
+        [MaxLength(2, ErrorMessage = "UF não pode ter mais do que 2 caracteres")]
         public string Uf {get; set;}
 
+        [Required(ErrorMessage = "Campo cidade é obrigatorio")]
         public string City {get; set;}
 
+        [Required(ErrorMessage = "Campo referência é obrigatorio")]
         public string Reference {get; set;}
 
+        [Required(ErrorMessage = "Campo descrição é obrigatorio")]
+        [MaxLength(100, ErrorMessage = "Campo descrição não pode ter mais do que 100 caracteres")]
         public string Description {get; set;}
     }
 }
